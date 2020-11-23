5dd3136e40671a157f1e37b77b6ff1a1d5068eb2
xjm <326308290@qq.com>
2020-11-23 11:40:22 +0800

    添加项目文件。

---
+++ src/MonitorStr.Sample/MonitorStr.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MonitorStr.Sample
+{
+    /**
+	 * 描述：
+	 * 
+	 * Author：xuejiaming
+	 * Created: 2020/11/23 11:06:27
+	 **/
+    public class MonitorStr
+    {
+        private MonitorStr() { }
+        private static ConcurrentDictionary<string, MonitorStrEntry> _lockDics = new ConcurrentDictionary<string, MonitorStrEntry>();
+        private const int _concurrentCount = 31;
+        private static object[] _lockers = new object[_concurrentCount];
+
+        static MonitorStr()
+        {
+            for (int i = 0; i < _concurrentCount; i++)
+            {
+                _lockers[i] = new object();
+            }
+        }
+        private static int GetIndex(string key)
+        {
+            return Math.Abs(key.GetHashCode() % _concurrentCount);
+        }
+        public static bool TryEnter(string key, int timeoutMillis)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key));
+            MonitorStrEntry entry = null;
+            var locker = _lockers[GetIndex(key)];
+            lock (locker)
+            {
+                if (!_lockDics.TryGetValue(key, out entry))
+                {
+                    entry = new MonitorStrEntry();
+                    _lockDics[key] = entry;
+                }
+                entry.Increment();
+            }
+
+            var acquired = Monitor.TryEnter(entry, timeoutMillis);
+            if (!acquired)
+                entry.Decrement();
+            return acquired;
+        }
+
+        public static void Exit(string key)
+        {
+            var entry = _lockDics[key];
+            Monitor.Exit(entry);
+            if (entry.Decrement() == 0)
+            {
+                var locker = _lockers[GetIndex(key)];
+                lock (locker)
+                {
+                    if (entry.CanRemove())
+                    {
+                        _lockDics.TryRemove(key, out var v);
+                    }
+                }
+            }
+        }
+        class MonitorStrEntry
+        {
+            private int _lockCount;
+
+            public int Increment()
+            {
+                Interlocked.Increment(ref _lockCount);
+                return _lockCount;
+            }
+
+            public int Decrement()
+            {
+                Interlocked.Decrement(ref _lockCount);
+                return _lockCount;
+            }
+            public bool CanRemove()
+            {
+                return _lockCount == 0;
+            }
+        }
+
+    }
+}
---
+++ src/MonitorStr.Sample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitorStr.Sample
+{
+    class Program
+    {
+        /// <summary>
+        /// cache
+        /// </summary>
+        private static ConcurrentDictionary<string,string> _cache=new ConcurrentDictionary<string, string>();
+        static void Main(string[] args)
+        {
+            var keys = Enumerable.Range(1, 200).ToList();
+            //mock request
+            for (int i = 0; i < 200000; i++)
+            {
+                Task.Run(() =>
+                {
+                    var key = keys[new Random().Next(1, 200)].ToString();
+                    //if key not in cache
+                    if (!_cache.ContainsKey(key))
+                    {
+                        //lock key with 3000 ms
+                        var acquired = MonitorStr.TryEnter(key, 3000);
+                        if (acquired)
+                        {
+                            try
+                            {
+                                //second query cache if not in cache
+                                if (!_cache.ContainsKey(key))
+                                {
+                                    Console.WriteLine($"key:{key} find some thing");
+                                    //mock query database
+                                    Thread.Sleep(200);
+                                    _cache[key]= key;
+                                    Console.WriteLine($"key:{key} find some thing complete");
+                                }
+                            }
+                            finally
+                            {
+                                MonitorStr.Exit(key);
+                                Console.WriteLine($"key:{key} exit");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"key:{key} lock time out");
+                            return;
+                        }
+                    }
+
+                    Console.WriteLine($"key:{key},value:{_cache[key]}");
+                });
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
